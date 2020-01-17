3e4509c66e3b0af50af08e7faefead1891be7ee3
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-17 16:13:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(file)
 
   emoticons_file.each { |key,value|
       hash[:get_meaning][value[1]] = key
-      hash[:get_emoticon][value[0]] = key
+      hash[:get_emoticon][value[0]] = value[1]
 
     }
 
